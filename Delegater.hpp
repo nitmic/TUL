589fbcf319f83f6e8a26697a3bcd27fc59604741
Takamoto <togari.takamoto@gmail.com>
2013-05-19 19:24:11 +0900

    first

---
+++ Delegater.hpp
@@ -0,0 +1,28 @@
+#pragma once
+template<class T>
+class Delegater{
+public:
+	void setDelegateObject(std::shared_ptr<T> & object){
+		delegate_object = object;
+	}
+	void releaseDelegateObject(){
+		delegate_object.reset();
+	}
+	T * getDelegateObject(){
+		return delegate_object.get();
+	}
+	T * operator->(){
+		return getDelegateObject();
+	}
+	T * operator*(){
+		return getDelegateObject();
+	}
+private:
+	std::shared_ptr<T> delegate_object;
+};
+
+template<class T>
+void ReleaseDelegater(T * p){
+	p->releaseDelegateObject();
+	delete p;
+}
---
+++ Singleton.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+template<class T>
+class Singleton{
+public:
+	static std::shared_ptr<T> & getInstance(){
+		if(m_spInstance == nullptr){
+			m_spInstance = std::make_shared<T>();
+		}
+		return m_spInstance;
+	}
+protected:
+	Singleton();
+	virtual ~Singleton(){
+		m_spInstance = nullptr;
+	}
+private:
+	Singleton(const Singleton& obj);
+	Singleton& operator=(const Singleton& obj);
+	static std::shared_ptr<T> m_spInstance;
+};
+
+template<class T>
+std::shared_ptr<T> Singleton<T>::m_spInstance = nullptr;
+
+template<class T>
+std::shared_ptr<T> & GetSingleton(){
+	return Singleton<T>::getInstance();
+}
