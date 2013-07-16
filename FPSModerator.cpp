09e760c8153aa2c15b9d0b4fe5ceab62a26485cb
takamoto <tkfmnkn@gmail.com>
2013-07-16 15:04:41 +0900

    iterator追加RingBufferに
    FPSModerator bugfix

--- FPSModerator.cpp
+++ FPSModerator.cpp
@@ -1,23 +1,33 @@
 #include "FPSModerator.h"
 #include <boost\chrono.hpp>
-
+#include "RingBuffer.hpp"
+#include <algorithm>
+/*
+#include <Windows.h>
+#include <mmsystem.h>
+#pragma comment(lib, "winmm.lib")
+*/
 namespace TUL{
+	enum{BufferSize = 30};
 	struct FPSModerator::Impl{
 		Impl(unsigned int fps=60) : frame(fps){}
 		boost::chrono::steady_clock::time_point lastTime;
-		unsigned int currentFPS;
+		RingBuffer<unsigned int, BufferSize> currentFPSs;
 		unsigned int frame;	//ê›íËFPS
+		boost::chrono::duration<int_least64_t, boost::nano> errorTime;
 	};
 	FPSModerator::FPSModerator(unsigned int settingFPS){
 		__impl__ = std::make_shared<Impl>(settingFPS);
 	}
 	bool FPSModerator::step(){
 		auto currentTime  = boost::chrono::high_resolution_clock::now();
-		auto pastTime     = (currentTime - __impl__->lastTime);
+		auto pastTime     = (currentTime - __impl__->lastTime) + __impl__->errorTime;
 		auto frameBase = boost::chrono::milliseconds(1000/__impl__->frame);
+
 		if(frameBase <= pastTime){
 			__impl__->lastTime = currentTime;
-			__impl__->currentFPS = boost::chrono::milliseconds(1000) / pastTime;
+			__impl__->currentFPSs.update(boost::chrono::milliseconds(1000) / pastTime);
+			__impl__->errorTime = pastTime % frameBase;
 			return true;
 		}
 		return false;
@@ -26,6 +36,10 @@ namespace TUL{
 		__impl__->frame = fps;
 	}
 	unsigned int FPSModerator::getActualFPS(){
-		return __impl__->currentFPS;
+		int sum = 0;
+		for(auto it=__impl__->currentFPSs.begin(); it!=__impl__->currentFPSs.end(); it++){
+			sum += *it;
+		}
+		return sum/BufferSize;
 	}
 };
--- RingBuffer.hpp
+++ RingBuffer.hpp
@@ -1,11 +1,12 @@
 #pragma once
 #include <array>
 #include "Error.h"
+#include <boost\iterator\iterator_facade.hpp>
 
 /*
 *@class RingBuffer
 *@ˆÈ‘O‚Ìó‘Ô‚ð‚¿‚å‚Á‚Æ‚ÌŠÔ•ÛŽ‚µ‚Ä‚¨‚«‚½‚¢‚à‚Ì‚É
-*  ˆË‘¶ TUL::Error
+*  ˆË‘¶ TUL::Error boost::iterator_facade
 */
 
 namespace TUL{
@@ -27,8 +28,12 @@ namespace TUL{
 		T & operator->(){
 			return current();
 		}
-
+		
 		void update(T & data){
+			update();
+			m_Data[m_CurrentIndex] = data;
+		}
+		void update(T && data){
 			update();
 			m_Data[m_CurrentIndex] = data;
 		}
@@ -38,13 +43,29 @@ namespace TUL{
 
 
 		T & prev(unsigned int x = 1){
-			if(x>N) ERROR_MSG(_T("x‚ª‘å‚«‚·‚¬‚Ü‚·"));
+			if(x>=N) ERROR_MSG(_T("x‚ª‘å‚«‚·‚¬‚Ü‚·"));
 			int preIndex = (m_CurrentIndex-x)%N;
 			if(preIndex<0){
-				preIndex = N - preIndex;
+				preIndex += N;
 			}
 			return m_Data[preIndex];
 		}
+
+
+		class iterator: public boost::iterator_facade<iterator, T, boost::forward_traversal_tag>{
+			public:
+				iterator(RingBuffer<T,N> * pBase,unsigned int index) : pBase(pBase), index(index){}
+			private:
+				friend class boost::iterator_core_access;
+				void increment() { index++; }
+				T& dereference() const {return pBase->prev(index);}
+				bool equal(const iterator& other) const { return index == other.index; }
+			private:
+				unsigned int index;
+				RingBuffer<T,N> * pBase;
+		};
+		iterator begin(){ return iterator(this, 0);}
+		iterator end() { return iterator(this, N);}
 	private:
 		int m_CurrentIndex;
 		std::array<T, N> m_Data;
